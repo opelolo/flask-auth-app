584f410eca85d825ccbf2349c3a4640498c3fd58
Opetunde Adepoju <109819000+opelolo@users.noreply.github.com>
2022-08-21 23:21:43 +0100

    Add post method to signup

--- project/auth.py
+++ project/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 from . import db
 
 auth = Blueprint('auth', __name__)
@@ -10,6 +10,11 @@ def login():
 @auth.route('/signup')
 def signup():
     return render_template('signup.html')
+
+@auth.route('/signup', methods=['POST'])
+def signup_post():
+    # code to validate and add user to database goes here
+    return redirect(url_for('auth.login'))
 
 @auth.route('/logout')
 def logout():
